f05e65bf5910a849ab537260a2ec090964f7ff88
Stefan Heimberg <kontakt@stefanheimberg.ch>
2015-07-02 23:52:19 +0200

    projekt setup

---
+++ example-fullscreen/src/main/java/example/App.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2015 Stefan Heimberg <kontakt@stefanheimberg.ch>.
+ * 
+ * All rights reserved.
+ */
+package example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Stefan Heimberg <kontakt@stefanheimberg.ch>
+ */
+public class App extends Application {
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        
+    }
+    
+}
