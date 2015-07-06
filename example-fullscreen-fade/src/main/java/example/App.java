98c33677f3163267dfa4f71ae02f4a6b371522f4
Stefan Heimberg <kontakt@stefanheimberg.ch>
2015-07-07 00:03:59 +0200

    kleinere verbesserungen

--- example-fullscreen-fade/src/main/java/example/App.java
+++ example-fullscreen-fade/src/main/java/example/App.java
@@ -5,12 +5,14 @@
  */
 package example;
 
+import java.io.IOException;
 import java.io.InputStream;
 import javafx.animation.FadeTransition;
 import javafx.animation.SequentialTransition;
-import javafx.animation.SequentialTransitionBuilder;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -35,19 +37,23 @@ public class App extends Application {
         launch(args);
     }
 
+    private static int currentImageNr = -1;
+
     @Override
     public void start(final Stage primaryStage) throws Exception {
         primaryStage.setTitle("Fullscreen Fade Example");
         primaryStage.setResizable(true);
 
         final ImageView iv = new ImageView();
+        iv.setPreserveRatio(true);
+        iv.setSmooth(true);
         iv.fitHeightProperty().bind(primaryStage.heightProperty());
         iv.fitWidthProperty().bind(primaryStage.widthProperty());
 
         final Pane root = new Pane();
         root.getChildren().add(iv);
 
-        final Scene scene = new Scene(root, 800, 600);
+        final Scene scene = new Scene(root, 1280/3*2, 720/3*2);
         scene.setFill(Color.WHITE);
 
         final KeyCodeCombination fullscreenKeyCodeCombination = new KeyCodeCombination(KeyCode.F, KeyCombination.SHORTCUT_DOWN);
@@ -80,34 +86,44 @@ public class App extends Application {
     }
 
     private void setImage(final int nr, final ImageView iv) {
-        final ClassLoader cl = getClass().getClassLoader();
-        final InputStream imageIS = cl.getResourceAsStream(String.format(IMAGE, nr));
-
-        if (null == imageIS) {
-            // nothing to set. image does not exists
-            return;
-        }
-
         final Image oldImage = iv.getImage();
-        final Image newImage = new Image(imageIS);
+        final Image newImage;
 
-        if (oldImage == null) {
-            // nothing to fade...
-            iv.setImage(newImage);
-            return;
+        if (currentImageNr == nr) {
+            newImage = oldImage;
+        } else {
+            currentImageNr = nr;
+            final ClassLoader cl = getClass().getClassLoader();
+            try (final InputStream imageIS = cl.getResourceAsStream(String.format(IMAGE, nr))) {
+                if (null == imageIS) {
+                    // nothing to set. image does not exists
+                    return;
+                }
+                newImage = new Image(imageIS, 1280, 720, true, true);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
 
-        final FadeTransition fadeOutTransition = new FadeTransition(Duration.seconds(1), iv);
+        if (oldImage == null || oldImage == newImage) {
+            iv.setImage(newImage);
+        } else {
+            fade(iv, (ActionEvent event) -> {
+                iv.setImage(newImage);
+            });
+        }
+    }
+
+    private void fade(final Node node, EventHandler<ActionEvent> finishAction) {
+        final FadeTransition fadeOutTransition = new FadeTransition(Duration.seconds(1), node);
         fadeOutTransition.setFromValue(1.0);
         fadeOutTransition.setToValue(0.3);
-        fadeOutTransition.setOnFinished((ActionEvent arg0) -> {
-            iv.setImage(newImage);
-        });
+        fadeOutTransition.setOnFinished(finishAction);
 
-        final FadeTransition fadeInTransition = new FadeTransition(Duration.seconds(1), iv);
+        final FadeTransition fadeInTransition = new FadeTransition(Duration.seconds(1), node);
         fadeInTransition.setFromValue(0.3);
         fadeInTransition.setToValue(1.0);
-        
+
         final SequentialTransition sequentialTransition = new SequentialTransition(fadeOutTransition, fadeInTransition);
         sequentialTransition.play();
     }
