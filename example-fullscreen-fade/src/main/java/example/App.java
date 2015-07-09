828daf80f372b2af01b66b1c44a6d81ff84c19b8
Stefan Heimberg <kontakt@stefanheimberg.ch>
2015-07-09 21:03:06 +0200

    vorbereitung für movie / image hin-und-her test

--- example-fullscreen-fade/src/main/java/example/App.java
+++ example-fullscreen-fade/src/main/java/example/App.java
@@ -14,6 +14,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -21,6 +25,9 @@ import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -46,43 +53,66 @@ public class App extends Application {
         primaryStage.setTitle("Fullscreen Fade Example");
         primaryStage.setResizable(true);
 
+        // https://blog.codecentric.de/en/2015/04/tweaking-the-menu-bar-of-javafx-8-applications-on-os-x
+        // https://github.com/codecentric/NSMenuFX
         final ImageView iv = new ImageView();
         iv.setPreserveRatio(true);
         iv.setSmooth(true);
         iv.fitHeightProperty().bind(primaryStage.heightProperty());
         iv.fitWidthProperty().bind(primaryStage.widthProperty());
-
-        final Pane root = new Pane();
-        root.getChildren().add(iv);
-
-        final Scene scene = new Scene(root, SCREEN_WIDTH/3*2, SCREEN_HEIGHT/3*2);
-        scene.setFill(Color.WHITE);
-
-        final KeyCodeCombination fullscreenKeyCodeCombination = new KeyCodeCombination(KeyCode.F, KeyCombination.SHORTCUT_DOWN);
-
-        scene.getAccelerators().put(fullscreenKeyCodeCombination, () -> {
+        
+        final MenuItem natGeo1MenuItem = new MenuItem("National Geographics 1");
+        natGeo1MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT1, KeyCombination.SHORTCUT_DOWN));
+        natGeo1MenuItem.setOnAction((ActionEvent event) -> {
+            setImage(1, iv);
+        });
+        
+        final MenuItem natGeo2MenuItem = new MenuItem("National Geographics 2");
+        natGeo2MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT2, KeyCombination.SHORTCUT_DOWN));
+        natGeo2MenuItem.setOnAction((ActionEvent event) -> {
+            setImage(2, iv);
+        });
+        
+        final MenuItem natGeo3MenuItem = new MenuItem("National Geographics 3");
+        natGeo3MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT3, KeyCombination.SHORTCUT_DOWN));
+        natGeo3MenuItem.setOnAction((ActionEvent event) -> {
+            setImage(3, iv);
+        });
+        
+        final MenuItem natGeo4MenuItem = new MenuItem("National Geographics 4");
+        natGeo4MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT4, KeyCombination.SHORTCUT_DOWN));
+        natGeo4MenuItem.setOnAction((ActionEvent event) -> {
+            setImage(4, iv);
+        });
+        
+        final MenuItem fullscreen = new MenuItem("Fullscreen");
+        fullscreen.setAccelerator(new KeyCodeCombination(KeyCode.F, KeyCombination.SHORTCUT_DOWN));
+        fullscreen.setOnAction((ActionEvent event) -> {
             primaryStage.setFullScreen(!primaryStage.isFullScreen());
         });
+        
+        final Menu mediaMenu = new Menu("Media");
+        mediaMenu.getItems().add(natGeo1MenuItem);
+        mediaMenu.getItems().add(natGeo2MenuItem);
+        mediaMenu.getItems().add(natGeo3MenuItem);
+        mediaMenu.getItems().add(natGeo4MenuItem);
+        mediaMenu.getItems().add(new SeparatorMenuItem());
+        
+        final Menu windowMenu = new Menu("Window");
+        windowMenu.getItems().add(fullscreen);
+        
+        final MenuBar menuBar = new MenuBar(mediaMenu, windowMenu);
+        menuBar.useSystemMenuBarProperty().set(true);
+        
+        final Pane root = new Pane();
+        root.getChildren().add(menuBar);
+        root.getChildren().add(iv);
 
-        scene.setOnKeyPressed((KeyEvent event) -> {
-            switch (event.getCode()) {
-                case DIGIT1:
-                    setImage(1, iv);
-                    break;
-                case DIGIT2:
-                    setImage(2, iv);
-                    break;
-                case DIGIT3:
-                    setImage(3, iv);
-                    break;
-                case DIGIT4:
-                    setImage(4, iv);
-                    break;
-            }
-        });
+        final Scene scene = new Scene(root, SCREEN_WIDTH / 3 * 2, SCREEN_HEIGHT / 3 * 2);
+        scene.setFill(Color.WHITE);
 
         setImage(1, iv);
-
+        
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -114,6 +144,15 @@ public class App extends Application {
                 iv.setImage(newImage);
             });
         }
+    }
+    
+    private void setMovie(final int nr, final MediaView mv) {
+        final MediaPlayer mediaPlayer = mv.getMediaPlayer();
+        
+        final Media oldMedia = mediaPlayer.getMedia();
+        final Media newMedia;
+        
+        // FIXME implement
     }
 
     private void fade(final Node node, EventHandler<ActionEvent> finishAction) {
