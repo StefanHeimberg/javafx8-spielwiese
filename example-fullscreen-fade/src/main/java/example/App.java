8dca10befffe6aefdf51f998c6501d089360629e
Stefan Heimberg <kontakt@stefanheimberg.ch>
2015-08-18 16:08:50 +0200

    some refactorings

--- example-fullscreen-fade/src/main/java/example/App.java
+++ example-fullscreen-fade/src/main/java/example/App.java
@@ -10,8 +10,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
+import javafx.animation.Transition;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
@@ -24,10 +28,10 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -38,109 +42,87 @@ public class App extends Application {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
 
+    private static final String DEFAULT = "media/default.jpg";
     private static final String IMAGE = "media/image%s.jpg";
     private static final String MOVIE = "media/movie%s.mp4";
 
-    private static final int SCREEN_WIDTH = 1280;
-    private static final int SCREEN_HEIGHT = 720;
-
+    private static final int MAX_SCREEN_WIDTH = 1280;
+    private static final int MAX_SCREEN_HEIGHT = 720;
+    
     public static void main(String[] args) {
         launch(args);
     }
 
-    private ImageView imageView;
-    private MediaView mediaView1;
-    private MediaView mediaView2;
+    private final ImageView imageView = new ImageView();
+    private final MediaView mediaView1 = new MediaView();
+    private final MediaView mediaView2 = new MediaView();
+    
     private Node prevNode;
     private MediaView currentMediaView;
     private MediaView nextMediaView;
-
+    
     @Override
     public void start(final Stage primaryStage) throws Exception {
         primaryStage.setTitle("Fullscreen Fade Example");
-        primaryStage.setResizable(true);
 
-        imageView = new ImageView();
-        imageView.setPreserveRatio(true);
-        imageView.setSmooth(true);
+        final MenuBar menuBar = new MenuBar(
+                new Menu("Media", null,
+                        createMenuItem("Image 1", KeyCode.DIGIT1, (ActionEvent event) -> {
+                            setImage(loadImage(String.format(IMAGE, 1)));
+                        }),
+                        createMenuItem("Image 2", KeyCode.DIGIT2, (ActionEvent event) -> {
+                            setImage(loadImage(String.format(IMAGE, 2)));
+                        }),
+                        new SeparatorMenuItem(),
+                        createMenuItem("Movie 1", KeyCode.DIGIT3, (ActionEvent event) -> {
+                            setMedia(loadMedia(String.format(MOVIE, 1)));
+                        }),
+                        createMenuItem("Movie 2", KeyCode.DIGIT4, (ActionEvent event) -> {
+                            setMedia(loadMedia(String.format(MOVIE, 2)));
+                        })
+                ),
+                new Menu("Window", null,
+                        createMenuItem("Fullscreen", KeyCode.F, (ActionEvent event) -> {
+                            primaryStage.setFullScreen(!primaryStage.isFullScreen());
+                        })
+                )
+        );
+        menuBar.setUseSystemMenuBar(true);
+
+        // Resize Binding 
         imageView.fitHeightProperty().bind(primaryStage.heightProperty());
         imageView.fitWidthProperty().bind(primaryStage.widthProperty());
-
-        mediaView1 = new MediaView();
-        mediaView1.setPreserveRatio(true);
-        mediaView1.setSmooth(true);
         mediaView1.fitHeightProperty().bind(primaryStage.heightProperty());
         mediaView1.fitWidthProperty().bind(primaryStage.widthProperty());
-
-        mediaView2 = new MediaView();
-        mediaView2.setPreserveRatio(true);
-        mediaView2.setSmooth(true);
         mediaView2.fitHeightProperty().bind(primaryStage.heightProperty());
         mediaView2.fitWidthProperty().bind(primaryStage.widthProperty());
+        
+        // Mediaplayer Binding
+        //        imageView.imageProperty().addListener((ObservableValue<? extends Image> observable, Image oldValue, Image newValue) -> {
+        //            final MediaPlayer mediaPlayer1 = mediaView1.getMediaPlayer();
+        //            if(null != mediaPlayer1 && mediaPlayer1.getStatus() == MediaPlayer.Status.PLAYING) {
+        //                mediaPlayer1.stop();
+        //            }
+        //            final MediaPlayer mediaPlayer2 = mediaView2.getMediaPlayer();
+        //            if(null != mediaPlayer2 && mediaPlayer2.getStatus() == MediaPlayer.Status.PLAYING) {
+        //                mediaPlayer2.stop();
+        //            }
+        //        });
+        
 
         currentMediaView = mediaView1;
         nextMediaView = mediaView2;
 
-        final MenuItem image1MenuItem = new MenuItem("Image 1");
-        image1MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT1, KeyCombination.SHORTCUT_DOWN));
-        image1MenuItem.setOnAction((ActionEvent event) -> {
-            setImage(loadImage(1));
-        });
+        final Pane pane = new StackPane();
+        pane.getChildren().add(menuBar);
+        pane.getChildren().add(imageView);
+        pane.getChildren().add(mediaView1);
+        pane.getChildren().add(mediaView2);
 
-        final MenuItem image2MenuItem = new MenuItem("Image 2");
-        image2MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT2, KeyCombination.SHORTCUT_DOWN));
-        image2MenuItem.setOnAction((ActionEvent event) -> {
-            setImage(loadImage(2));
-        });
+        setImage(loadImage(DEFAULT));
 
-        final MenuItem movie1MenuItem = new MenuItem("Movie 1");
-        movie1MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT3, KeyCombination.SHORTCUT_DOWN));
-        movie1MenuItem.setOnAction((ActionEvent event) -> {
-            setMedia(loadMedia(1));
-        });
-
-        final MenuItem movie2MenuItem = new MenuItem("Movie 2");
-        movie2MenuItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT4, KeyCombination.SHORTCUT_DOWN));
-        movie2MenuItem.setOnAction((ActionEvent event) -> {
-            setMedia(loadMedia(2));
-        });
-
-        final MenuItem fullscreen = new MenuItem("Fullscreen");
-        fullscreen.setAccelerator(new KeyCodeCombination(KeyCode.F, KeyCombination.SHORTCUT_DOWN));
-        fullscreen.setOnAction((ActionEvent event) -> {
-            primaryStage.setFullScreen(!primaryStage.isFullScreen());
-        });
-
-        final Menu mediaMenu = new Menu("Media");
-        mediaMenu.getItems().add(image1MenuItem);
-        mediaMenu.getItems().add(image2MenuItem);
-        mediaMenu.getItems().add(new SeparatorMenuItem());
-        mediaMenu.getItems().add(movie1MenuItem);
-        mediaMenu.getItems().add(movie2MenuItem);
-
-        final Menu windowMenu = new Menu("Window");
-        windowMenu.getItems().add(fullscreen);
-
-        final MenuBar menuBar = new MenuBar(mediaMenu, windowMenu);
-        menuBar.useSystemMenuBarProperty().set(true);
-
-        final Scene scene = new Scene(new Pane(menuBar, imageView, mediaView1, mediaView2), SCREEN_WIDTH / 3 * 2, SCREEN_HEIGHT / 3 * 2);
-        scene.setFill(Color.WHITE);
-
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(pane, MAX_SCREEN_WIDTH / 3 * 2, MAX_SCREEN_HEIGHT / 3 * 2));
         primaryStage.show();
-    }
-
-    private Image loadImage(final int nr) {
-        try (final InputStream imageIS = new FileInputStream(String.format(IMAGE, nr))) {
-            if (null == imageIS) {
-                // nothing to set. image does not exists
-                return null;
-            }
-            return new Image(imageIS, SCREEN_WIDTH, SCREEN_HEIGHT, true, true);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
     }
 
     private void setImage(final Image image) {
@@ -160,11 +142,6 @@ public class App extends Application {
         prevNode = imageView;
     }
 
-    private Media loadMedia(final int nr) {
-        final File mediaFile = new File(String.format(MOVIE, nr));
-        return new Media(mediaFile.toURI().toString());
-    }
-
     private void setMedia(final Media media) {
         final Media oldMedia;
         if (null == currentMediaView.getMediaPlayer()) {
@@ -180,7 +157,7 @@ public class App extends Application {
             mediaPlayer.setMute(true);
             mediaPlayer.setAutoPlay(true);
             mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-            
+
             currentMediaView.setMediaPlayer(mediaPlayer);
         } else {
             new FadeTransitionBuilder()
@@ -193,6 +170,29 @@ public class App extends Application {
         prevNode = nextMediaView;
         nextMediaView = currentMediaView;
         currentMediaView = (MediaView) prevNode;
+    }
+    
+    private static Image loadImage(final String filePath) {
+        try (final InputStream imageIS = new FileInputStream(filePath)) {
+            if (null == imageIS) {
+                // nothing to set. image does not exists
+                return null;
+            }
+            return new Image(imageIS, MAX_SCREEN_WIDTH, MAX_SCREEN_HEIGHT, true, true);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    private static Media loadMedia(final String mediaPath) {
+        return new Media(new File(mediaPath).toURI().toString());
+    }
+
+    private static MenuItem createMenuItem(final String label, final KeyCode keyCode, final EventHandler<ActionEvent> eventHandler) {
+        final MenuItem menuItem = new MenuItem(label);
+        menuItem.setAccelerator(new KeyCodeCombination(keyCode, KeyCombination.SHORTCUT_DOWN));
+        menuItem.setOnAction(eventHandler);
+        return menuItem;
     }
 
 }
